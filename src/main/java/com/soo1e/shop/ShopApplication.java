1d3c61e76f7c146b7ca1f6172cdfd20c1cf3c158
soo1e <gameswuon@gmail.com>
2024-08-28 14:52:50 +0900

    초기 세팅

---
+++ src/main/java/com/soo1e/shop/ShopApplication.java
@@ -0,0 +1,13 @@
+package com.soo1e.shop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShopApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/soo1e/shop/ShopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.soo1e.shop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
